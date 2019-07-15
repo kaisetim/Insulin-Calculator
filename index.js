b79eeaaf2465ba19aea752b3d5befe468577d2db
Kaiser <Tim.Kaiser@adidas.com>
2019-07-15 11:04:32 +0200

    dummy html with res.send

--- index.js
+++ index.js
@@ -3,6 +3,39 @@ const dotenv = require("dotenv").config(),
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send(`<!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <title>Homepage</title>
+    </head>
+    <body>
+        <h1>Title</h1>
+
+        <form>
+            <label>
+                Morning: 
+                <input type="text" name="morning">
+            </label>
+            <label>
+                Midday: 
+                <input type="text" name="midday">
+            </label>
+            <label> 
+                Evening: 
+                <input type="text" name="evening">
+            </label>
+            <button type="submit">Submit</button>
+        </form>
+    </body>
+  </html>
+  `);
+});
+
+app.get("*", (req, res) => {
+  res.send("URL not found ...");
+});
+
 app.listen(process.env.PORT, () =>
   console.log(`Server is listening on PORT ${process.env.PORT} ...`)
 );
